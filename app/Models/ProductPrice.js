e2e2c48c1794e5dbc92fc397713287f1914e41fd
Breno França <franciscobreno.si@gmail.com>
2019-08-09 01:48:34 -0300

    product_price: adicionado formato de apresentacao

--- app/Models/ProductPrice.js
+++ app/Models/ProductPrice.js
@@ -5,6 +5,17 @@ const Model = use("Model");
 class ProductPrice extends Model {
   static boot() {
     super.boot();
+
+    this.addHook("afterFetch", async items => {
+      for (let item of items) {
+        const price_currency = (item.price / 100)
+          .toFixed(2)
+          .replace(".", ",")
+          .replace(/(\d)(?=(\d{3})+\,)/g, "$1.");
+
+        item.price_presentation = `R$ ${price_currency}`;
+      }
+    });
   }
 
   product() {
