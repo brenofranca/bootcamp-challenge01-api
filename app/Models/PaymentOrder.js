a80756d59a999bee70f3467cdc59112bd3992f99
Breno França <franciscobreno.si@gmail.com>
2019-08-14 00:43:45 -0300

    formata valor do pedido

--- app/Models/PaymentOrder.js
+++ app/Models/PaymentOrder.js
@@ -3,6 +3,18 @@
 const Model = use("Model");
 
 class PaymentOrder extends Model {
+  static boot() {
+    super.boot();
+
+    this.addHook("afterFetch", async items => {
+      for (let item of items) {
+        item.price = (item.price / 100)
+          .toFixed(2)
+          .replace(".", ",")
+          .replace(/(\d)(?=(\d{3})+\,)/g, "$1.");
+      }
+    });
+  }
   products() {
     return this.hasMany("App/Models/PaymentOrderProduct");
   }
